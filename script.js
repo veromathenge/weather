eb25aa6fa6db41d883d1f8e70cd2d66f912b7977
veromathenge <85177314+veromathenge@users.noreply.github.com>
2024-10-27 20:07:56 +0300

    added advance search engine

--- script.js
+++ script.js
@@ -3,7 +3,39 @@ function weatherChange(response) {
   let temperature = response.data.temperature.current;
   temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#city");
+  let conditionElement = document.querySelector("#condition-unit");
+  let humidityElement = document.querySelector("#humidity-unit");
+  let windElement = document.querySelector("#wind-unit");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
   cityElement.innerHTML = "📍" + response.data.city;
+
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/hr`;
+  timeElement.innerHTML = formatDate(date);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 function submitCity(city) {
   let apiKey = "3o0f81ct132b1a0b35fd66fb04863244";
