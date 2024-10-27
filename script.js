d2754c5ea13987f4eaaaf8105f2dcf833fd43a21
veromathenge <85177314+veromathenge@users.noreply.github.com>
2024-10-27 16:19:53 +0300

    added search engine

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+function weatherChange(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = "📍" + response.data.city;
+}
+function submitCity(city) {
+  let apiKey = "3o0f81ct132b1a0b35fd66fb04863244";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(weatherChange);
+}
+
 function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  submitCity(searchInput.value);
 }
 let selectButton = document.querySelector("#search-form");
 selectButton.addEventListener("submit", searchCity);
+
+submitCity("Nairobi");
